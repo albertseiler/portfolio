90f8d41babd2433240dcdc8e9f0d05d5ea842b14
Albert-Alexandru Seiler <113704524+albertseiler@users.noreply.github.com>
2023-06-26 16:24:38 +0200

    Update More.jsx

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -12,7 +12,7 @@ const More = forwardRef(function More(props, ref,) {
                 <p>I am a self taught front-end developer, who is passionate about building awesome websites and applications! I have currently learned and used HTML, CSS, Javascript and React. On personal level, I am self-driven, hard working, I am able to adapt very quick and overall I am very curious. Also, I am a really friendly person, who can fit in any team.
                     <br />
                     <br />
-                    <span className='myskills'> My current skills:</span>
+                    <div className='myskills'> My current skills:</div>
                     <br />
                     <br />
                     HTML, CSS, TAILWIND, Javascript, React, GIT

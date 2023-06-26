521fce541e4f8c529bb860845dfff622579fb8f0
Albert-Alexandru Seiler <113704524+albertseiler@users.noreply.github.com>
2023-06-26 15:56:33 +0200

    added source code for the webiste

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -3,7 +3,7 @@ const ContactMe = () => {
         <div className="contact-container">
             <h2>Would you like to hire me ? <br /> Awesome!</h2>
             <a href="mailto:seileralbertalexandru@gmail.com"><button className="contact-btn hvr-bounce-to-right">seileralbertalexandru@gmail.com</button></a>
-            <a href=""><button className="contact-btn hvr-bounce-to-right">source code for this website</button></a>
+            <a href="https://github.com/albertseiler/portfolio" target="_blank" rel="noreferrer"><button className="contact-btn hvr-bounce-to-right">source code for this website</button></a>
         </div>
     )
 }
